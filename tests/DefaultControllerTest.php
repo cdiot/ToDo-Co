57d3f0ab9aca6dbe3d42d77938dcb18fbc83d589
cdiot <christopher.diot5@gmail.com>
2021-12-12 14:13:58 +0100

    Add DefaultControllerTest

---
+++ tests/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testShouldDisplayHomepage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !');
+    }
+}
