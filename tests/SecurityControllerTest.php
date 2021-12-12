4b208fa523db5cea58a6aa372fb91eab56ea2113
cdiot <christopher.diot5@gmail.com>
2021-12-12 14:14:26 +0100

    Add SecurityControllerTest

---
+++ tests/SecurityControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testShouldDisplayLogin(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('button', 'S\'identifier');
+    }
+
+    public function testVisitingWhileLoggedIn()
+    {
+        $client = static::createClient();
+
+        // get or create the user somehow (e.g. creating some users only
+        // for tests while loading the test fixtures)
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('foo@gmail.com');
+
+        $client->loginUser($testUser);
+
+        // user is now logged in, so you can test protected resources
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !');
+    }
+}
